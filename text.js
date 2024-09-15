5a869a0b955120367dff18461fa8ea0783c2eab3
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-09-16 03:53:18 +0600

    complite the Second Function

--- text.js
+++ text.js
@@ -1,5 +1,5 @@
 
-// No 01.
+// No 01. Help The Zoo Manager To Calculate Income Of Zoo
 function calculateMoney(ticketSale) {
     if (typeof ticketSale !== 'number') {
         return "Invalid Number : It's Not A Number";
@@ -30,15 +30,24 @@ function calculateMoney(ticketSale) {
 }
 
 
-// NO 02.
+
+// NO 02. Identify Good Name or Bad Name
+function checkName(name) {
+    if (typeof name !== "string") {
+        return "Invalid String : It's Not A String";
+    }
+    const lowerName = name.toLowerCase();
+    if ( lowerName[(lowerName.length - 1)] =="a" || lowerName[(lowerName.length - 1)] =="y" || lowerName[(lowerName.length - 1)] =="i" || lowerName[(lowerName.length - 1)] =="e" || lowerName[(lowerName.length - 1)] =="o" || lowerName[(lowerName.length - 1)] =="u" || lowerName[(lowerName.length - 1)] =="w" ){
+        return "Good Name";
+    }
+    else{
+        return "Bad Name";
+    }
+}
 
 
 
-
-
-
-
-
+//  No 03. 
 
 
 
